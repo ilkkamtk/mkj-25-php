aa294312e5dbce2f1b474fe67ba43076a9f37733
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-04-03 14:32:51 +0300

    add main structure with header, footer, and media management components

---
+++ viikko3/PDO-harjoitus/media-table.php
@@ -0,0 +1,22 @@
+<section id="media-items">
+    <table>
+        <thead>
+        <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Created</th>
+            <th>Owner</th>
+            <th>Thumbnail</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Lorem</td>
+            <td>Ipsum öalkjsdhf kajshdf</td>
+            <td>12.5.2002</td>
+            <td>Seppo</td>
+            <td><img src="" alt=""></td>
+        </tr>
+        </tbody>
+    </table>
+</section>
