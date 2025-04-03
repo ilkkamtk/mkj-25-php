591fb8ea3853ff0e3bbf3af6eef434186dcd7cd0
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-04-03 15:14:23 +0300

    add database connection and data selection functionality

--- viikko3/PDO-harjoitus/inc/media-table.php
+++ viikko3/PDO-harjoitus/inc/media-table.php
@@ -10,13 +10,9 @@
         </tr>
         </thead>
         <tbody>
-        <tr>
-            <td>Lorem</td>
-            <td>Ipsum öalkjsdhf kajshdf</td>
-            <td>12.5.2002</td>
-            <td>Seppo</td>
-            <td><img src="" alt=""></td>
-        </tr>
+        <?php
+        require_once __DIR__ . '/../operations/selectData.php';
+        ?>
         </tbody>
     </table>
 </section>
---
+++ viikko3/PDO-harjoitus/operations/selectData.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__ . '/../db/dbConnect.php';
+?>
+
+<tr>
+    <td>Lorem</td>
+    <td>Ipsum öalkjsdhf kajshdf</td>
+    <td>12.5.2002</td>
+    <td>Seppo</td>
+    <td><img src="" alt=""></td>
+</tr>
