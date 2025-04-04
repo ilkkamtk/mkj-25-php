22c1720c2a0445f7f1ec9d0006d0fca66b649666
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-04-04 13:55:16 +0300

    add user authentication forms and implement media item deletion functionality

---
+++ viikko3/PDO-harjoitus/inc/login-form.php
@@ -0,0 +1,14 @@
+<section id="register-form">
+    <h2>Login</h2>
+    <form action="">
+        <div class="form-control">
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username"/>
+        </div>
+        <div class="form-control">
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password"/>
+        </div>
+        <button type="submit">Register</button>
+    </form>
+</section>
---
+++ viikko3/PDO-harjoitus/inc/register-form.php
@@ -0,0 +1,18 @@
+<section id="register-form">
+    <h2>Register</h2>
+    <form action="">
+        <div class="form-control">
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username"/>
+        </div>
+        <div class="form-control">
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password"/>
+        </div>
+        <div class="form-control">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email"/>
+        </div>
+        <button type="submit">Register</button>
+    </form>
+</section>
