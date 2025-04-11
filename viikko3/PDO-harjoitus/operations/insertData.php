e560aab6bbdef3ef29bd6b77f53c4a4df6c9a865
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-04-11 14:56:04 +0300

    add brute force attack simulation page and enhance file upload validation

--- viikko3/PDO-harjoitus/operations/insertData.php
+++ viikko3/PDO-harjoitus/operations/insertData.php
@@ -20,15 +20,30 @@ if (!empty($_POST['title']) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
     $title = $_POST['title'];
     $description = $_POST['description'];
     $user_id = $_SESSION['user']['user_id'];
-/*
+
     // vain kuvia ja videoita
     $allowed_types = array('image/jpeg', 'image/png', 'image/gif',
         'image/webp', 'video/mp4', 'video/webm', 'video/ogg', 'video/mov');
     if (!in_array($filetype, $allowed_types)) {
         exit('Invalid file type.');
     }
-*/
-    if (move_uploaded_file($tmp_name, $destination)) {
+
+	// check file size
+	$max_size = 1024 * 1024 * 8; // 8MB
+
+	if ($filesize > $max_size) {
+		echo "File too large";
+		exit;
+	}
+
+	// double check that file does not contain php
+	if ( str_contains( $filename, '.php' ) ) {
+		echo "Invalid file name";
+		exit;
+	}
+
+
+	if (move_uploaded_file($tmp_name, $destination)) {
         echo "File uploaded successfully";
     } else {
         echo "Error uploading file";
